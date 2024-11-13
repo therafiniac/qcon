9fd0c2e6682b64c99009926101636d1eb133896f
therafiniac <therafiniac@gmail.com>
2024-11-13 13:17:35 +0530

    Create initial folder structure according to 7-1 sass architecture andsome dev dependecies tools

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 // Import our custom CSS
-import "../scss/styles.scss";
+import "../scss/main.scss";
 
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { resolve } from "path";
+import purgecss from "vite-plugin-purgecss";
 
 export default {
   root: resolve(__dirname, "src"),
   build: {
     outDir: "../dist",
   },
+  plugins: [
+    purgecss({
+      content: ["./**/*.html", "./src/**/*.js"], // Paths to check for used classes
+    }),
+  ],
   server: {
     port: 8080,
   },
